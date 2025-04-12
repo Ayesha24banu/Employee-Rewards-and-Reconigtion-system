288488761b96e3864813016c225c6e7c7b7bcea3
Ayesha24banu <167692806+Ayesha24banu@users.noreply.github.com>
2025-04-12 15:49:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,25 +3,25 @@ import os
 
 load_dotenv()
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', '\x0f\x0e\x8f\x96\xfaaX+\xbc\x9d+\xbb5\xc9Lo')
+    SECRET_KEY = os.getenv('SECRET_KEY', 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx')
     MYSQL_HOST = os.getenv('MYSQL_HOST', 'localhost')
-    MYSQL_USER = os.getenv('MYSQL_USER', 'root')
-    MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD', 'Ayesha@990')
+    MYSQL_USER = os.getenv('MYSQL_USER', 'xxxxxx')
+    MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD', 'xxxxxxxxxxxx')
     MYSQL_DB = os.getenv('MYSQL_DB', 'employee_reward')
     MYSQL_CURSORCLASS = 'DictCursor'
     FLASK_DEBUG = os.getenv('FLASK_DEBUG')
     
     # Email configuration
     MAIL_SERVER = os.getenv('MAIL_SERVER', 'smtp.gmail.com')
-    MAIL_PORT = int(os.getenv('MAIL_PORT', '587'))
+    MAIL_PORT = int(os.getenv('MAIL_PORT', 'xxx'))
     MAIL_USE_TLS = os.getenv('MAIL_USE_TLS', 'True') 
     MAIL_USE_SSL = os.getenv('MAIL_USE_SSL', 'False')
-    MAIL_USERNAME = os.getenv('MAIL_USERNAME', 'aneesayesha35@gmail.com')
-    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD', 'sqei eioy bjcc wywu ')
-    MAIL_DEFAULT_SENDER = os.getenv('MAIL_DEFAULT_SENDER', 'aneesayesha35@gmail.com')
+    MAIL_USERNAME = os.getenv('MAIL_USERNAME', 'xxxxxxxxxxxx@gmail.com')
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD', 'xxxx xxxx xxxx xxxx ')
+    MAIL_DEFAULT_SENDER = os.getenv('MAIL_DEFAULT_SENDER', 'xxxxxxxxx@gmail.com')
     
     # Security
-    SECURITY_PASSWORD_SALT = os.getenv('SECURITY_PASSWORD_SALT', '04626b2326b3f38b44ab76a08dbc8bdb')
+    SECURITY_PASSWORD_SALT = os.getenv('SECURITY_PASSWORD_SALT', 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx')
 
 class TestConfig(Config):
     MYSQL_DB = 'test_employee_reward'
